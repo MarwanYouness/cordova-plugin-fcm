f8058e2262844340ef35ef41a25e37001f54d488
Marwan Youness <marwan.h.youness@gmail.com>
2018-06-02 14:29:02 +0200

    Update MyFirebaseMessagingService.java

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build.VERSION;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import java.util.Map;
