09f2e85b239db4b2b493089bbc51de32a3953f6b
Marwan Youness <marwan.h.youness@gmail.com>
2018-06-10 16:52:29 +0200

    remove notification channel for testing

--- src/android/MyFirebaseMessagingService.java
+++ src/android/MyFirebaseMessagingService.java
@@ -1,12 +1,12 @@
 package com.gae.scaffolder.plugin;
-import android.app.NotificationChannel;
+//import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
-import android.os.Build;
+//import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import java.util.Map;
@@ -81,14 +81,12 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-	  if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-
-	      /* Create or update. */
+	  /*if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
 	      NotificationChannel channel = new NotificationChannel("magnamylife_channel",
 		  "push_channel", 
 		  NotificationManager.IMPORTANCE_DEFAULT);
 	      notificationManager.createNotificationChannel(channel);
-	  }
+	  } */
 
         notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
